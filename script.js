6b6893cc3d7b0137614ddbefa7ee9f86aea8304e
angel <108492882+angel-472@users.noreply.github.com>
2022-07-15 18:46:35 -0400

    Work more on calculator design and behavior

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const firstNumberDisplay = document.querySelector('.first-number');
 const secondNumberDisplay = document.querySelector('.second-number');
 const operatorDisplay = document.querySelector('.operator');
 
-let input = 0;
+let input = '0';
 let currentOperator = null;
 let firstNumber = null;
 let secondNumber = null;
@@ -55,15 +55,22 @@ function niceOperator(operator){
 }
 
 function updateDisplay(){
-  inputDisplay.innerText = parseInt(input).toLocaleString("en-US");
+  if(input.length < 1){
+    input = '0';
+  }
+  updateOperatorButtons();
+  inputDisplay.innerText = parseFloat(input).toLocaleString("en-US");
+  firstNumberDisplay.innerText = firstNumber !== null ? firstNumber.toLocaleString("en-US") : "";
+  secondNumberDisplay.innerText = secondNumber !== null ? secondNumber.toLocaleString("en-US") : "";
+  operatorDisplay.innerText = currentOperator ? niceOperator(currentOperator) : "";
+}
+
+function updateOperatorButtons(){
   ['/','*','-','+','='].forEach(id => {
     if(currentOperator == id) return;
     let element = document.getElementById(id);
     element.classList.remove('selected-operator');
   });
-  firstNumberDisplay.innerText = firstNumber ? firstNumber.toLocaleString("en-US") : "";
-  secondNumberDisplay.innerText = secondNumber ? secondNumber.toLocaleString("en-US") : "";
-  operatorDisplay.innerText = currentOperator ? niceOperator(currentOperator) : "";
 }
 
 //number buttons
@@ -92,22 +99,24 @@ for(i = 0; i <= 9; i++){
   document.getElementById(id).addEventListener('click', event => {
     let element = event.currentTarget;
 
-    if(!firstNumber){
-      firstNumber = parseInt(input);
-      input = 0;
-    } else if(secondNumber) {
-      firstNumber = secondNumber;
-      secondNumber = parseInt(input);
-      showResult();
-    } else {
-      secondNumber = parseInt(input);
-      showResult();
+    if(element.id == "="){
+      if(firstNumber !== null && currentOperator !== null && input !== null) {
+        secondNumber = parseFloat(input);
+        showResult();
+        console.log('showing result');
+      }
+      console.log('end');
+      return;
     }
 
-    if(element.id == "="){
-      if(firstNumber && currentOperator && secondNumber) {
-        showResult();
-      }
+    if(firstNumber == null){
+      firstNumber = parseFloat(input);
+      input = '0';
+      console.log('first number');
+    } else {
+      secondNumber = parseFloat(input);
+      showResult();
+      console.log('second number');
       return;
     }
     currentOperator = element.id;
@@ -118,7 +127,7 @@ for(i = 0; i <= 9; i++){
 
 //calculate and display operation result
 function updateLastOperation(){
-  if(firstNumber && currentOperator && secondNumber){
+  if(firstNumber !== null && currentOperator !== null && secondNumber !== null){
     firstNumberDisplay.innerText = firstNumber.toLocaleString("en-US");
     secondNumberDisplay.innerText = secondNumber.toLocaleString("en-US");
     operatorDisplay.innerText = niceOperator(currentOperator);
@@ -131,21 +140,39 @@ function updateLastOperation(){
 
 function showResult(){
   let result = operate(firstNumber,currentOperator,secondNumber);
-  if(result == undefined) return;
+  if(!Number.isInteger(result)) result = 0;
+  console.log('result: ' + result + ' ' + secondNumber);
   input = result;
-  inputIsResult = true;
-  currentOperator = null;
-  firstNumber = 0;
-  secondNumber = null;
   updateDisplay();
   updateLastOperation();
+  inputIsResult = true;
+  currentOperator = null;
+  updateOperatorButtons();
+  firstNumber = null;
+  secondNumber = null;
 }
 
+//undo button
+document.getElementById("undo").addEventListener('click', event => {
+  if(input.length < 1 || input == 0) return;
+  input = input.substring(0, input.length - 1);
+  updateDisplay();
+});
+
+//clear button
+document.getElementById("clear").addEventListener('click', event => {
+  input = '0';
+  currentOperator =  null;
+  firstNumber = null;
+  secondNumber = null;
+  updateDisplay();
+});
 
 //keyboard support
 window.addEventListener('keydown',event => {
-  console.log(event);
-  let button = document.querySelector(`button[data-key="${event.key}"]`);
+  let key = event.key;
+  if(key == "Enter") key = "=";
+  let button = document.querySelector(`button[data-key="${key}"]`);
   if(!button) return;
   let e = new Event('click');
   button.dispatchEvent(e);
